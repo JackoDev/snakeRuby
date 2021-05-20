ee40b4993806726702d84d0443503f8ce18aa299
JackoDev <untaljacko@gmail.com>
2021-05-19 22:02:54 -0500

    initial commit for exercise

---
+++ src/model/state.rb
@@ -0,0 +1,40 @@
+# Representation of the game state
+
+module Model
+
+  module Direction
+    UP = :up
+    RIGHT = :right
+    LEFT = :left
+    DOWN = :down
+  end
+
+
+  class Coord < Struct.new(:row, :col)
+  end
+
+  class Food < Coord
+  end
+
+  class Snake < Struct.new(:positions)
+  end
+
+  class Grid < Struct.new(:rows, :cols)
+  end
+
+  class State < Struct.new(:snake, :food, :grid, :next_direction)
+  end
+
+  def self.initial_state
+    Model::State.new(
+      Model::Snake.new([
+        Model::Coord.new(1,1),
+        Model::Coord.new(0,1),
+      ]),
+      Model::Food.new(4,4),
+      Model::Grid.new(8,12),
+      Direction::DOWN
+    )
+  end
+
+end
